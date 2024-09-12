efbeb1f73c4b039a9f4b66617b75132f15733fbc
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-09-12 13:41:33 +0300

    Start Onboarding Screen
    *added images and svgs folder for assets
    *added flutter_svg && flutter_screen_util packages
    *added go_router package navigator
    *added color & text styles
    *added empty login_screen folder and files
    *added onboarding screen

---
+++ lib/features/login/ui/login_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tap_doc_app/features/login/ui/screens/login_view_body.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LoginViewBody();
+  }
+}
---
+++ lib/features/onboarding/ui/widgets/get_started_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tap_doc_app/core/router/app_router.dart';
+import 'package:tap_doc_app/core/theme/colors.dart';
+
+import '../../../../core/theme/styles.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        GoRouter.of(context).push(AppRouter.kLogin);
+      },
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(ColorsManager.mainBlue),
+        minimumSize: WidgetStateProperty.all(
+          const Size(double.infinity, 52),
+        ),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+      child: Text(
+        'Get Started',
+        style: Styles.font16WhiteSemiBold,
+      ),
+    );
+  }
+}
