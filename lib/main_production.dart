bb3e441549e1302edc458c67c4cda0ce9ba4161b
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-10-01 21:28:19 +0300

    Flavors Android
    - add launch.json for flavors config
    - added Development & Production Flavors for Android
    - changed Android App name for each flavor

---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:tap_doc_app/tap_doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/di/dependency_injection.dart';
+
+void main() async{
+  setupGetIt();
+  // To Fix texts being hidden bug in release mode
+  await ScreenUtil.ensureScreenSize();
+  runApp(
+    const TapDocApp(),
+  );
+}
